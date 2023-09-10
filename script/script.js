8c572f40a53b865bad2a3ae9b805176f8406d120
Akash <akkyjat601@gmail.com>
2023-09-10 13:10:58 +0530

    Visual Layout

---
+++ script/script.js
@@ -0,0 +1,7 @@
+let container = document.querySelector(".container");
+let upper = document.querySelector(".upper");
+let ball = document.querySelector(".ball");
+let lower = document.querySelector(".lower");
+
+console.log(ball.getBoundingClientRect());
+console.log(lower.getBoundingClientRect());
